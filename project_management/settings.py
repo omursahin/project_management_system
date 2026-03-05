ae7123d1b442473fb637b9799f0243fd8b670c73
baris karvan <1030521178@erciyes.edu.tr>
2026-03-05 15:45:59 +0300

    added cors settings to settings.py

--- project_management/settings.py
+++ project_management/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -118,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+CORS_ALLOWED_ORIGINS = [
+    "*"
+]
+
+
