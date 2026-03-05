460a9144d0b704f35f36585970b3fed51bd8daba
kumasyaren1 <yaren5433@gmail.com>
2026-03-05 19:05:04 +0300

    Django ile bağlantı

--- project_management/settings.py
+++ project_management/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = "project_management.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / 'frontend' / 'build'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -109,3 +109,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'frontend' / 'build' / 'static',
+]
