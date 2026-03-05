8465a00105971cf74ec24b8e63237c7d784edfc0
kumasyaren1 <yaren5433@gmail.com>
2026-03-05 16:22:02 +0300

    layout

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,14 @@
-import { Provider } from "@/components/ui/provider" // v3 snippet yapısı
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <Provider>
+    {/* İşte sihirli dokunuş burada: value={defaultSystem} */}
+    <ChakraProvider value={defaultSystem}>
       <App />
-    </Provider>
+    </ChakraProvider>
   </React.StrictMode>
-)
+);
