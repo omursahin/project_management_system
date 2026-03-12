bbc740e40dcc93aa810ace6aa466cc1233ab9460
Mehmet <150680956+Mhmtshn6@users.noreply.github.com>
2026-03-12 16:42:52 +0300

    Test.py düzeldi

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,53 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+User = get_user_model()
+
+
+class AuthTests(APITestCase):
+    def setUp(self):
+        self.username = "testuser"
+        self.password = "TestSifresi123!"
+        self.user = User.objects.create_user(
+            username=self.username,
+            password=self.password
+        )
+
+        self.login_url = reverse('token_obtain_pair')
+
+
+    def test_kullanici_girisi_basarili(self):
+        """Kullanıcı doğru bilgilerle giriş yaptığında token (200 OK) almalıdır."""
+        data = {
+            "username": self.username,
+            "password": self.password
+        }
+        response = self.client.post(self.login_url, data)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('access', response.data)
+        self.assertIn('refresh', response.data)
+
+    def test_kullanici_girisi_basarisiz(self):
+        """Kullanıcı yanlış şifre girdiğinde giriş yapamamalıdır."""
+        data = {
+            "username": self.username,
+            "password": "YanlisSifre!"
+        }
+        response = self.client.post(self.login_url, data)
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+    """
+    def test_kullanici_cikisi(self):
+        # Önce giriş yapıp token alalım
+        login_response = self.client.post(self.login_url, {"username": self.username, "password": self.password})
+        refresh_token = login_response.data['refresh']
+
+        # Sonra o token ile çıkış (blacklist) isteği atalım
+        response = self.client.post(self.logout_url, {"refresh": refresh_token})
+        self.assertEqual(response.status_code, status.HTTP_205_RESET_CONTENT)
+    """
