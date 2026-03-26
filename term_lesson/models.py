6347f0a390ce34317190b7f9d584f59319b1138b
enes <enesodunkesen@hotmail.com>
2026-03-26 16:32:09 +0300

    Update models and migrations for term_lesson and term_lesson_student

--- term_lesson/models.py
+++ term_lesson/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class TermLesson(models.Model):
+    id = models.BigAutoField(primary_key=True)
+
+    def __str__(self):
+        return f"TermLesson {self.id}"
--- term_lesson_student/models.py
+++ term_lesson_student/models.py
@@ -1,3 +1,23 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class TermLessonStudent(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    student = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="term_lesson_students",
+    )
+    term_lesson = models.ForeignKey(
+        "term_lesson.TermLesson",
+        on_delete=models.CASCADE,
+        related_name="term_lesson_students",
+    )
+    midterm = models.IntegerField()
+    final = models.IntegerField()
+    make_up = models.IntegerField()
+    is_approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.student} - {self.term_lesson}"
