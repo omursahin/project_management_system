40caeb8a8055ffc9d43cf408f2d231e14f196d6c
ahmet <ahmethassancelik@gmail.com>
2026-03-26 14:38:16 +0100

    main.jsx is updated for react query integration

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { QueryClientProvider } from '@tanstack/react-query'
 import App from './App.jsx'
 import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
-import { queryClient } from './lib/queryClient.js'
+import { queryClient } from './queryClient.js'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
