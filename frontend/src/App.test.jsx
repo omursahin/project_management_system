89ecfad3e239df504d3117efb6ea65973dc355b2
Abdulvahap Mete <vahapmt@gmail.com>
2026-03-26 16:40:11 +0300

    Update App.test.jsx to use defaultSystem in ChakraProvider

--- frontend/src/App.test.jsx
+++ frontend/src/App.test.jsx
@@ -1,10 +1,11 @@
 import { render, screen } from '@testing-library/react';
-import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
+import { defaultSystem } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 
 test('renders app component without crashing', () => {
   render(
-    <ChakraProvider>
+    <ChakraProvider value={defaultSystem}>
       <App />
     </ChakraProvider>
   );
