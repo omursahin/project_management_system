cc5f00bfea1f18fff22eeb13b4d27f71a83eb7f6
ALAN MAHMUD <alan.mahmoud777@gmail.com>
2026-04-02 15:44:43 +0300

    grouProject modeli olusturdum.

--- group_project/models.py
+++ group_project/models.py
@@ -1,19 +1,10 @@
 from django.db import models
 
-
 class GroupProject(models.Model):
-
     group = models.ForeignKey('group.Group', on_delete=models.CASCADE, related_name='group_projects')
-
     title = models.CharField(max_length=200)
-
-
     description = models.CharField(max_length=1000)
-
     status = models.CharField(max_length=50)
-
-
     is_approved = models.BooleanField(default=False)
-
     def __str__(self):
         return f"{self.title} - {self.group}"
